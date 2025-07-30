a5fe7f170511cb2b09ac2287999ab7e68c66b8ec
Raksha162004 <raksharao162004@gmail.com>
2025-07-30 09:09:37 +0530

    add

---
+++ linked_list.py
@@ -0,0 +1,32 @@
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+
+class LinkedList:
+    def __init__(self):
+        self.head=None
+
+    def add_data(self,data):
+        newnode=Node(data)
+        if not self.head:
+            self.head=newnode
+        else:
+            temp=self.head
+            while(temp.next!=None):
+                temp=temp.next
+            temp.next=newnode
+
+    def display(self):
+        temp=self.head
+        while temp:
+            print(temp.data,end="->")
+            temp=temp.next
+        print("None")
+
+l=LinkedList()
+l.add_data(20)
+l.add_data(10)
+l.add_data(15)
+l.add_data(28)
+l.display()
